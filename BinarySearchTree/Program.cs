51f774e96815e30f9251ddb970ad21c82fb6dca7
SalmanLS <salmanluthfi456@gmail.com>
2022-12-26 17:52:11 +0700

    create menu and call all the object to run the program

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -116,6 +116,58 @@ namespace BinarySearchTree
                 Console.WriteLine(ptr.info+"");
             }
         }
+
+        static void Main(string[] args)
+        {
+            BinaryTree x = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("\n Menu");
+                Console.WriteLine("1. Implement insert operation");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine("5. Exit");
+                Console.Write("\n Enter your choice (1-5) : ");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.WriteLine("Enter a word: ");
+                            string word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inOrder(x.ROOT);
+                        }
+                        break;
+                    case '3':
+                        {
+                            x.preOrder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postOrder(x.ROOT);
+                        }
+                        break;
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("Invalid Option");
+                            break;
+
+                        }
+                        
+                }   
+
+            }
+        }
     }
     
 
