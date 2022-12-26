2cbda054699ab36a00c669b3b9835ee038679023
SalmanLS <salmanluthfi456@gmail.com>
2022-12-26 17:26:50 +0700

    create insert method if the specified node is not present

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace BinarySearchTree
 {
@@ -30,6 +31,33 @@ namespace BinarySearchTree
             ROOT = null; // initializing root to null
         }
 
+        public void insert(string element) // insert a node in the binary 
+        {
+            Node tmp, parent = null, currentNode = null;
+            find(element, ref parent, ref currentNode);
+            if (currentNode != null)
+            {
+                Console.WriteLine("Duplicate words not allowed");
+                return;
+            }
+            else // if the specified node is not present 
+            {
+                tmp = new Node(element, null, null);
+                if (parent == null)
+                {
+                    ROOT= tmp;
+                }
+                else if (string.Compare(element, parent.info) <0)
+                {
+                    if(string.Compare(element, parent.info) <0)
+                        parent.lchild= tmp;
+                }
+                else
+                {
+                    parent.rchild= tmp;
+                }
+            }
+        }
     }
     
 
